365c8751e844eab41216fd43f542fb96cd5b4854
wingzero <wingzzz2003@hotmail.com>
2015-07-14 17:55:08 +0800

    add crawl time control flag; revise updatePage.php to reserve the mnemono fields

--- disposableCodes/addMacauLocation.js
+++ disposableCodes/addMacauLocation.js
@@ -12,3 +12,16 @@ db.FacebookPage.find(
     db.FacebookPage.save(e);
 });
 
+use Mnemono;
+db.FacebookPage.find({"mnemono.location.city":"mo"}).forEach(function(e){
+    print(e._id);
+    e.mnemono.crawlTime = [0, 6, 12, 18];
+    db.FacebookPage.save(e);
+});
+
+use Mnemono;
+db.FacebookPage.find({"mnemono.location.city":"hk"}).forEach(function(e){
+    print(e._id);
+    e.mnemono.crawlTime = [5, 11, 17, 23];
+    db.FacebookPage.save(e);
+});
