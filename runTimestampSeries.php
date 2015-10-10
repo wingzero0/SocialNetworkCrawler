3c68e5d09fdf276f06082e4db788593a8bf18c71
wingzero <wingzzz2003@hotmail.com>
2015-10-10 14:18:24 +0800

    revert hard coding time range

--- runTimestampSeries.php
+++ runTimestampSeries.php
@@ -28,8 +28,7 @@ if (isset($argv[3])){
     $city = "mo";
 }
 
-//$endDate = new \DateTime();
-$endDate = \DateTime::createFromFormat(\DateTime::ISO8601, "2015-09-23T00:00:00+0000");
+$endDate = new \DateTime();
 $startDate = clone $endDate;
 $startDate->sub(new \DateInterval('P'.$windowSize.'D'));
 
