bdbff0c067176cfd2298e1577b548b9f2fa5da30
wingzero <wingzzz2003@hotmail.com>
2015-10-15 21:40:07 +0800

    change insertNewPage input content pattern

--- disposableCodes/dumpPageInfo.php
+++ disposableCodes/dumpPageInfo.php
@@ -18,37 +18,54 @@ $cursor = $col->find(array( "\$or" => array(
 )));
 
 echo "fbID". getDelimiter()
+    ."mnemonoCat". getDelimiter()
+    ."mnemonoCrawlTime".getDelimiter()
+    ."mnemonoCity".getDelimiter()
+    ."mnemonoCountry".getDelimiter()
     ."name". getDelimiter()
     ."likes". getDelimiter()
     ."link". getDelimiter()
     ."checkins". getDelimiter()
     ."were_here_count". getDelimiter()
-    ."mnemonoCat". getDelimiter()
-    ."crawlTime".getDelimiter()
     ."fbCategory". getDelimiter()
     ."fbCategory_list". getDelimiter()
     ."\n";
 foreach ($cursor as $pageRaw){
 	echo $pageRaw["fbID"].getDelimiter();
+    if (isset($pageRaw["mnemono"])){
+        if (isset($pageRaw["mnemono"]["category"])){
+            echo $pageRaw["mnemono"]["category"] .getDelimiter();
+        }else{
+            echo "NULL".getDelimiter();
+        }
+        if (isset($pageRaw["mnemono"]["crawlTime"]) && !empty($pageRaw["mnemono"]["crawlTime"])){
+            foreach($pageRaw["mnemono"]["crawlTime"] as $hour){
+                echo $hour . ",";
+            }
+            echo getDelimiter();
+        }else{
+            echo "NULL".getDelimiter();
+        }
+        if (isset($pageRaw["mnemono"]["location"])){
+            echo $pageRaw["mnemono"]["location"]["city"] . getDelimiter();
+            echo $pageRaw["mnemono"]["location"]["country"] . getDelimiter();
+        }else{
+            for($i = 0; $i<2 ;$i++) {
+                echo "NULL" . getDelimiter();
+            }
+        }
+    }else{
+        for($i = 0; $i<4 ;$i++){
+            echo "NULL".getDelimiter();
+        }
+    }
+
 	echo toString($pageRaw, "name").getDelimiter();
 	echo toString($pageRaw, "likes").getDelimiter();
 	echo toString($pageRaw, "link").getDelimiter();
 	echo toString($pageRaw, "checkins").getDelimiter();
 	echo toString($pageRaw, "were_here_count").getDelimiter();
-    if (isset($pageRaw["mnemono"]) && isset($pageRaw["mnemono"]["category"])){
-        echo $pageRaw["mnemono"]["category"] .getDelimiter();
-    }else{
-        echo "NULL".getDelimiter();
-    }
-    if (isset($pageRaw["mnemono"]) && isset($pageRaw["mnemono"]["crawlTime"])
-        && !empty($pageRaw["mnemono"]["crawlTime"])){
-        foreach($pageRaw["mnemono"]["crawlTime"] as $hour){
-           echo $hour . ",";
-        }
-        echo getDelimiter();
-    }else{
-        echo "NULL".getDelimiter();
-    }
+
 	echo toString($pageRaw, "category").getDelimiter();
 	if (isset($pageRaw["category_list"]) && !empty($pageRaw["category_list"])){
 		foreach ($pageRaw["category_list"] as $category){
